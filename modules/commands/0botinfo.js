d65b5e20743fc9a9545107cab220d12d57be00de
Md Masud Rana <118968969+Termuxmasud404@users.noreply.github.com>
2024-11-19 13:14:09 +0600

    Update 0botinfo.js

--- modules/commands/0botinfo.js
+++ modules/commands/0botinfo.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "botinfo",
 	version: "1.0.1", 
 	hasPermssion: 0,
-	credits: "Arun Kumar", //don't change the credits please
+	credits: " ᴹᴿ°᭄︵мAsυd⊹⊱ᴮᴼˢˢ", //don't change the credits please
 	description: " Bot info.",
 	commandCategory: "system",
 	cooldowns: 1,
