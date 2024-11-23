57c73744a96d6eeb724d4506b35b1a18af612b73
Md Masud Rana <118968969+Termuxmasud404@users.noreply.github.com>
2024-11-23 22:07:16 +0600

    Update and rename Arun1.js to Masud.js

--- modules/commands/Masud.js
+++ modules/commands/Masud.js
@@ -1,9 +1,9 @@
  const fs = require("fs");
 module.exports.config = {
-	name: "Arun",
+	name: "Masud",
     version: "1.0.1",
 	hasPermssion: 0,
-	credits: "Arun", 
+	credits: "Masud", 
 	description: "no prefix",
 	commandCategory: "No command marks needed",
 	usages: "...",
@@ -17,7 +17,7 @@ module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
      react.includes("Arun") || 
 react.includes("arun")) {
 		var msg = {
-				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦ᴹᴿ°᭄︵мAsυd⊹⊱ᴮᴼˢˢ✦\n\n★★᭄𝐘𝐨𝐮𝐭𝐮𝐛𝐞 𝐋𝐢𝐧𝐤 :\n\n✦ https://youtube.com/@MirryKal ✦ \n𝗝𝗼𝗶𝗻 𝗢𝘂𝗿 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 𝗚𝗿𝗼𝘂𝗽 \n 𝗞𝗮𝗮𝗹 𝗟𝗼𝗸 😋https://www.facebook.com/groups/207371140648761/?ref=share_group_link`",
+				body: "★𝗢𝘄𝗻𝗲𝗿 + 𝗠𝗮𝗱𝗲 𝗕𝘆★\n\n✦ᴹᴿ°᭄︵мAsυd⊹⊱ᴮᴼˢˢ✦\n`",
 				attachment: fs.createReadStream(__dirname + `/noprefix/kk1.jpg`)
 			}
 			api.sendMessage(msg, threadID, messageID);
